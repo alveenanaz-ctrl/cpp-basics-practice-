ad830c80cb5b8394c3bb78550a0aaae23137040f
Alveena Naz <nazalveena71@gmail.com>
2025-08-25 11:33:14 +0500

    variables.cpp
    This is the beginning of my learning c++ .

---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int a = 2;
+  int b = 8;
+  int product = a * b;
+  cout << "The product of a and b is:" << product << "/n";
+
+  return 0;
+}
